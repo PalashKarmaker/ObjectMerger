09cfe73ff88791923f0dcd934a34c71d7c73d6e4
Karmakar-EXT Palash (uie37359) <palash.2.karmakar-EXT@conti.de>
2022-07-18 10:07:08 +0000

    Add project files.

---
+++ ObjectMerger/PropertyMerger.cs
@@ -0,0 +1,33 @@
+﻿using System.Dynamic;
+
+namespace ObjectMerger;
+
+public class PropertyMerger
+{
+    public static ExpandoObject ObjectMerge<T1, T2>(T1 t1, T2 t2, string newClassName, IEnumerable<string>? t1ExcludedProperies = null, IEnumerable<string>? t2ExcludedProperies = null)
+    {
+        var ps = new Dictionary<string, object?>[] { GetProperties(t1, t1ExcludedProperies), GetProperties(t2, t2ExcludedProperies) };
+        var properties = ps.SelectMany(dict => dict);
+        return CreateDynamicObject(newClassName, properties);
+    }
+
+    public static Dictionary<string, object?> GetProperties<T>(T t, IEnumerable<string>? excludedProperies = null) =>
+        t.GetType().GetProperties()
+        .Where(p => excludedProperies == null || !excludedProperies.Contains(p.Name))
+        .ToDictionary(k => k.Name, v => v.GetValue(t));
+
+    public static ExpandoObject CreateDynamicObject(string className,
+    IEnumerable<KeyValuePair<string, object?>> properties)
+    {
+        dynamic obj = new ExpandoObject();
+        obj.ClassName = className;
+        AssignProperties(obj, properties);
+        return obj;
+    }
+
+    private static void AssignProperties(ExpandoObject obj, IEnumerable<KeyValuePair<string, object?>> properties)
+    {
+        foreach (var item in properties)
+            ((IDictionary<string, object?>)obj)[item.Key] = item.Value;
+    }
+}
---
+++ ObjectMergerTests/PropertyMergerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObjectMerger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMerger.Tests
+{
+    [TestClass()]
+    public class PropertyMergerTests
+    {
+        Model1 m1;
+        Model2 m2;
+        [TestInitialize]
+        public void Init()
+        {
+            m1 = new() { Name = "Boom" };
+            m2 = new() { Name2 = "Shaka Laka" };
+        }
+        [TestMethod()]
+        public void CreateDynamicObjectTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ObjectMergeTest()
+        {
+            dynamic obj = PropertyMerger.ObjectMerge(m1, m2, "Dhoom");
+            Assert.IsTrue(obj.Name == m1.Name);
+        }
+    }
+    public class Model1
+    {
+        public string Name { get; set; }
+    }
+    public class Model2
+    {
+        public string Name2 { get; set; }
+    }
+}
